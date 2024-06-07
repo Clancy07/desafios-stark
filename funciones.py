35d8f2fed203ebb8873f804b9fb1281db2dc257a
Santiago Centurion <clancy7007@gmail.com>
2024-06-07 15:15:30 -0300

    TP completo

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 import os
 
 
-def crear_menu(opciones):
+def crear_menu(opciones: list) -> str:
     """
     Crea un menu de opciones con las opciones recibidas y devuelve la opcion elegida por el usuario.
 
@@ -9,7 +9,7 @@ def crear_menu(opciones):
         opciones (list): Son las opciones del menu
 
     Returns:
-        char: Es la opcion elegida por el usuario
+        str: Es la opcion elegida por el usuario
     """
     for opcion in opciones:
         print(f'{opcion}')
@@ -31,6 +31,15 @@ def verificar_opcion(opcion: str, opciones_validas: list) -> True | False:
 
 
 def mapear_diccionarios(diccionarios: list, claves: list) -> list:
+    """Mapea una lista de diccionarios acorde a las claves recibidas
+
+    Args:
+        diccionarios (list): Lista de diccionarios a mapear
+        claves (list): Lista de las claves segun las cuales se mapearan los diccionarios
+
+    Returns:
+        list: Diccionarios mapeados acorde a las claves recividas
+    """
     diccionarios_mapeados = []
     diccionario_mapeado = {}
     for diccionario in diccionarios:
@@ -42,6 +51,14 @@ def mapear_diccionarios(diccionarios: list, claves: list) -> list:
 
 
 def duplicar_lista(lista_original: list) -> list:
+    """Recive una lista y crea una copia
+
+    Args:
+        lista_original (list): Lista a duplicar
+
+    Returns:
+        list: Copia de la lista original
+    """
     lista_duplicada = []
     for elemento in lista_original:
         lista_duplicada.append(elemento)
@@ -49,7 +66,15 @@ def duplicar_lista(lista_original: list) -> list:
     return lista_duplicada
 
 
-def normalizar_diccionarios(diccionarios):
+def normalizar_diccionarios(diccionarios: list) -> list:
+    """Normaliza los valores de una lista de diccionarios, intentando convertir los valores a números flotantes cuando sea posible.
+
+    Args:
+        diccionarios (list): lista de diccionarios cuyos valores se intentaran convertir a numeros flotantes
+
+    Returns:
+        list: diccionarios con los valores convertidos a flotantes en los casos en los que haya sido posible
+    """
     diccionarios_normalizados = []
 
     for diccionario in diccionarios:
@@ -68,7 +93,14 @@ def normalizar_diccionarios(diccionarios):
     return diccionarios_normalizados
 
 
-def mostrar_valores_por_claves(diccionarios, claves, espaciado: int = 12):
+def mostrar_valores_por_claves(diccionarios: list, claves: list, espaciado: int = 12) -> None:
+    """Muestra en formato de tabla los valores asociados a un conjunto de claves recividas para una lista de diccionarios.
+
+    Args:
+        diccionarios (list): Lista de diccionarios que contienen los datos a mostrar.
+        claves (list): Lista de claves cuyos valores asociados en los diccionarios se mostraran
+        espaciado (int, optional): Cantidad de esapacios que se utilizarán para formatear las columnas de la tabla. Defaults to 12.
+    """
     encabezados = []
     for clave in claves:
         encabezados.append(clave)
@@ -84,3 +116,252 @@ def mostrar_valores_por_claves(diccionarios, claves, espaciado: int = 12):
             print(f'{diccionario[clave]:^{espaciado}}', end='')
 
         print()
+
+
+def calcular_maximo_en_clave_diccionarios(diccionarios: list, clave: str) -> int:
+    """Calcula el maximo valor entre los valores asociados a una clave dentro de una lista de diccionarios.
+
+    Args:
+        diccionarios (list): Lista de diccionarios donde se buscara el maximo valor.
+        clave (str): Clave en la que se buscará el maximo valor los diccionarios.
+
+    Returns:
+        int: El maximo valor encontrado entre los valores asociados a la clave recivida.
+    """
+    maximo = diccionarios[0][clave]
+
+    for diccionario in diccionarios:
+        if diccionario[clave] > maximo:
+            maximo = diccionario[clave]
+
+    return maximo
+
+
+def calcular_minimo_en_clave_diccionarios(diccionarios: list, clave: str) -> int:
+    """Calcula el minimo valor entre los valores asociados a una clave dentro de una lista de diccionarios.
+
+    Args:
+        diccionarios (list): Lista de diccionarios donde se buscara el minimo valor.
+        clave (str): Clave en la que se buscará el minimo valor los diccionarios.
+
+    Returns:
+        int: El minimo valor encontrado entre los valores asociados a la clave recivida.
+    """
+    minimo = diccionarios[0][clave]
+
+    for diccionario in diccionarios:
+        if diccionario[clave] < minimo:
+            minimo = diccionario[clave]
+
+    return minimo
+
+
+def calcular_promedio_en_clave_diccionarios(diccionarios: list, clave: str) -> float:
+    """Calcula el promedio de los valores asociados a una clave recivida dentro de una lista de diccionarios.
+
+    Args:
+        diccionarios (list): Lista de diccionarios donde se buscaran los valores para calcular el promedio.
+        clave (str): Clave entre cuyos valores asociadosen se buscaran los valores para calcular el promedio en cada diccionario.
+
+    Returns:
+        float: El promedio de los valores calculado entre los valores asociados a la clave recivida de los diccionarios.
+    """
+    cantidad_diccionarios = len(diccionarios)
+    acumulador = 0
+    for diccionario in diccionarios:
+        acumulador += diccionario[clave]
+
+    promedio = acumulador / cantidad_diccionarios
+    return promedio
+
+
+def filtrar_lista_diccionarios(lista: list, clave: str, valor_buscado: any) -> list:
+    """Recorre una lista de diccionarios buscando coincidencias con el valor recivido en una clave especifica
+
+    Args:
+        lista (list): lista de diccionarios en la que se buscara el valor recivido
+        clave (str): Clave entre cuyos valores asociados se buscara el valor recivido
+        valor_buscado (any): Valor que se buscara en la lista de diccionarios
+
+    Returns:
+        list: Lista filtrada que solo contiene los diccionarios que contienen coincidencias con el valor buscado
+    """
+    lista_filtrada = []
+    for elemento in lista:
+        if elemento[clave] == valor_buscado:
+            lista_filtrada.append(elemento)
+
+    return lista_filtrada
+
+
+def determinar_colores_de_ojo(heroes_normalizados: list) -> None:
+    """Ordena la lista de heroes por color de ojos y los muestra
+
+    Args:
+        heroes_normalizados (list): Lista de heroes que se ordenara y mostrara segun el color de ojos
+    """
+
+    contador_colores_de_ojo = {}
+    contador_colores_de_ojo["Yellow"] = 0
+    contador_colores_de_ojo["Silver"] = 0
+    contador_colores_de_ojo["Brown"] = 0
+    contador_colores_de_ojo["Yellow (without irises)"] = 0
+    contador_colores_de_ojo["Blue"] = 0
+    contador_colores_de_ojo["Green"] = 0
+    contador_colores_de_ojo["Red"] = 0
+    contador_colores_de_ojo["Hazel"] = 0
+
+    for heroe in heroes_normalizados:
+        match(heroe["color_ojos"]):
+
+            case 'Yellow':
+                contador_colores_de_ojo["Yellow"] += 1
+
+            case 'Silver':
+                contador_colores_de_ojo["Silver"] += 1
+
+            case 'Brown':
+                contador_colores_de_ojo["Brown"] += 1
+
+            case 'Yellow (without irises)':
+                contador_colores_de_ojo["Yellow (without irises)"] += 1
+
+            case 'Blue':
+                contador_colores_de_ojo["Blue"] += 1
+
+            case 'Green':
+                contador_colores_de_ojo["Green"] += 1
+
+            case 'Red':
+                contador_colores_de_ojo["Red"] += 1
+
+            case 'Hazel':
+                contador_colores_de_ojo["Hazel"] += 1
+
+    for clave, valor in contador_colores_de_ojo.items():
+        print(f'{clave}: {valor}')
+
+
+def determinar_colores_de_pelo(heroes_normalizados):
+    """Ordena la lista de heroes por color de pelo y los muestra
+
+    Args:
+        heroes_normalizados (list): Lista de heroes que se ordenara y mostrara segun el color de pelo
+    """
+
+    contador_colores_de_pelo = {}
+    contador_colores_de_pelo["Black"] = 0
+    contador_colores_de_pelo["Auburn"] = 0
+    contador_colores_de_pelo["Brown"] = 0
+    contador_colores_de_pelo["White"] = 0
+    contador_colores_de_pelo["Green"] = 0
+    contador_colores_de_pelo["Brown / White"] = 0
+    contador_colores_de_pelo["Red / Orange"] = 0
+    contador_colores_de_pelo["Yellow"] = 0
+    contador_colores_de_pelo["No Hair"] = 0
+    contador_colores_de_pelo["Red"] = 0
+    contador_colores_de_pelo["Blond"] = 0
+
+    for heroe in heroes_normalizados:
+        match(heroe["color_pelo"]):
+
+            case 'Black':
+                contador_colores_de_pelo["Black"] += 1
+
+            case 'Auburn':
+                contador_colores_de_pelo["Auburn"] += 1
+
+            case 'Brown':
+                contador_colores_de_pelo["Brown"] += 1
+
+            case 'White':
+                contador_colores_de_pelo["White"] += 1
+
+            case 'Green':
+                contador_colores_de_pelo["Green"] += 1
+
+            case 'Brown / White':
+                contador_colores_de_pelo["Brown / White"] += 1
+
+            case 'Red / Orange':
+                contador_colores_de_pelo["Red / Orange"] += 1
+
+            case 'Yellow':
+                contador_colores_de_pelo["Yellow"] += 1
+
+            case 'No Hair':
+                contador_colores_de_pelo["No Hair"] += 1
+
+            case 'Red':
+                contador_colores_de_pelo["Red"] += 1
+
+            case 'Blond':
+                contador_colores_de_pelo["Blond"] += 1
+
+    for clave, valor in contador_colores_de_pelo.items():
+        print(f'{clave}: {valor}')
+
+
+def determinar_tipos_de_inteligencia(heroes_normalizados):
+    """Ordena la lista de heroes por tipo de inteligencia y los muestra
+
+    Args:
+        heroes_normalizados (list): Lista de heroes que se ordenara y mostrara segun el tipo de inteligencia
+    """
+    tipos_de_inteligencia = []
+
+    for heroe in heroes_normalizados:
+        tipos_de_inteligencia.append(heroe["inteligencia"])
+
+    tipos_de_inteligencia = list(set(tipos_de_inteligencia))
+
+    contador_colores_de_pelo = {}
+    contador_colores_de_pelo["No tiene"] = 0
+    contador_colores_de_pelo["average"] = 0
+    contador_colores_de_pelo["good"] = 0
+    contador_colores_de_pelo["high"] = 0
+
+    for heroe in heroes_normalizados:
+        match(heroe["inteligencia"]):
+
+            case '':
+                contador_colores_de_pelo["No tiene"] += 1
+
+            case 'average':
+                contador_colores_de_pelo["average"] += 1
+
+            case 'good':
+                contador_colores_de_pelo["good"] += 1
+
+            case 'high':
+                contador_colores_de_pelo["high"] += 1
+
+    for clave, valor in contador_colores_de_pelo.items():
+        print(f'{clave}: {valor}')
+
+
+def ordenar_diccionarios(criterio, diccionarios: list) -> None:
+    """Ordena una lista de diccionarios segun el criterio recivido
+
+    Args:
+        criterio (_type_): Criterio segun el cual se ordenara la lista
+        diccionarios (list): Lista de diccionarios a ordenar
+    """
+    tam = len(diccionarios)
+    for i in range(tam - 1):
+        for j in range(i + 1, tam):
+            if criterio(diccionarios[i], diccionarios[j]):
+                swap_lista(diccionarios, i, j)
+
+
+def swap_lista(lista: list, i: int, j: int) -> None:
+    """Swapea dos elementos de la lista recivida
+
+    Args:
+        lista (list): Lista cuyos elementos se swapearan
+        i (int): indice del primer elemento del swap
+        j (int): Indice del segundo elemento del swap
+    """
+    aux = lista[i]
+    lista[i] = lista[j]
+    lista[j] = aux
--- main.py
+++ main.py
@@ -8,6 +8,14 @@ heroes_normalizados = normalizar_diccionarios(heroes)
 
 ejecutando = True
 
+flag_3 = False
+flag_4 = False
+
+flag_10 = False
+flag_11 = False
+flag_12 = False
+flag_13 = False
+
 opciones_menu_principal = ['DESAFIO #00 ============================================================================================================',
                            '1. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe.',
                            '2. Recorrer la lista imprimiendo por consola nombre de cada superhéroe junto a la altura del mismo.',
@@ -19,21 +27,21 @@ opciones_menu_principal = ['DESAFIO #00 ========================================
                            '========================================================================================================================',
                            '',
                            'DESAFIO #01 ============================================================================================================',
-                           '10. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género M.',
-                           '11. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género F.',
-                           '12. Recorrer la lista y determinar cuál es el superhéroe más alto de género M.',
-                           '13. Recorrer la lista y determinar cuál es el superhéroe más alto de género F.',
-                           '14. Recorrer la lista y determinar cuál es el superhéroe más bajo de género M.',
-                           '15. Recorrer la lista y determinar cuál es el superhéroe más bajo de género F.',
-                           '16. Recorrer la lista y determinar la altura promedio de los superhéroes de género M.',
-                           '17. Recorrer la lista y determinar la altura promedio de los superhéroes de género F.',
-                           '18. Informar cual es el Nombre del superhéroe asociado a cada uno de los indicadores anteriores (ítems C a F).',
-                           '19. Determinar cuántos superhéroes tienen cada tipo de color de ojos.',
-                           '20. Determinar cuántos superhéroes tienen cada tipo de color de pelo.',
-                           '21. Determinar cuántos superhéroes tienen cada tipo de inteligencia (En caso de no tener, Inicializarlo con "No Tiene").',
-                           '22. Listar todos los superhéroes agrupados por color de ojos.',
-                           '23. Listar todos los superhéroes agrupados por color de pelo.',
-                           '24. Listar todos los superhéroes agrupados por tipo de inteligencia.',
+                           '8. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género M.',
+                           '9. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género F.',
+                           '10. Recorrer la lista y determinar cuál es el superhéroe más alto de género M.',
+                           '11. Recorrer la lista y determinar cuál es el superhéroe más alto de género F.',
+                           '12. Recorrer la lista y determinar cuál es el superhéroe más bajo de género M.',
+                           '13. Recorrer la lista y determinar cuál es el superhéroe más bajo de género F.',
+                           '14. Recorrer la lista y determinar la altura promedio de los superhéroes de género M.',
+                           '15. Recorrer la lista y determinar la altura promedio de los superhéroes de género F.',
+                           '16. Informar cual es el Nombre del superhéroe asociado a cada uno de los indicadores anteriores (ítems 10 al 13).',
+                           '17. Determinar cuántos superhéroes tienen cada tipo de color de ojos.',
+                           '18. Determinar cuántos superhéroes tienen cada tipo de color de pelo.',
+                           '19. Determinar cuántos superhéroes tienen cada tipo de inteligencia (En caso de no tener, Inicializarlo con "No Tiene").',
+                           '20. Listar todos los superhéroes agrupados por color de ojos.',
+                           '21. Listar todos los superhéroes agrupados por color de pelo.',
+                           '22. Listar todos los superhéroes agrupados por tipo de inteligencia.',
                            '========================================================================================================================',
                            'X. SALIR',
                            '']
@@ -68,7 +76,9 @@ while ejecutando:
             os.system('pause')
 
         case '3':
-            print('Heroe mas alto')
+            flag_3 = True
+
+            print('Heroes mas altos')
 
             alturas = mapear_diccionarios(heroes_normalizados, ['altura'])
             heroes_mas_altos = []
@@ -82,71 +92,260 @@ while ejecutando:
                 if heroe["altura"] == altura_maxima:
                     heroes_mas_altos.append(heroe)
 
-            mostrar_valores_por_claves(
-                heroes_mas_altos, [heroes_mas_altos[0].keys()])
+            for heroe in heroes_mas_altos:
+                print(f'Nombre: {heroe["nombre"]}. Altura: {heroe["altura"]}')
+
+            os.system('pause')
 
         case '4':
-            pass
+            flag_4 = True
+            print('Heroes mas bajos')
+
+            alturas = mapear_diccionarios(heroes_normalizados, ['altura'])
+            heroes_mas_bajos = []
+            altura_minima = alturas[0]["altura"]
+
+            for altura in alturas:
+                if altura["altura"] < altura_minima:
+                    altura_minima = altura["altura"]
+
+            for heroe in heroes_normalizados:
+                if heroe["altura"] == altura_minima:
+                    heroes_mas_bajos.append(heroe)
+
+            for heroe in heroes_mas_bajos:
+                print(f'Nombre: {heroe["nombre"]}. Altura: {heroe["altura"]}')
+
+            os.system('pause')
 
         case '5':
-            pass
+            promedio_alturas = calcular_promedio_en_clave_diccionarios(
+                heroes_normalizados, 'altura')
+
+            print(
+                f'\nEl promedio de alturas de los superheroes es de: {promedio_alturas:.2f}\n')
+            os.system('pause')
 
         case '6':
-            pass
+            if flag_3 and flag_4:
+                for heroe in heroes_mas_altos:
+                    print(
+                        f'Nombre: {heroe["nombre"]}. Altura: {heroe["altura"]}')
+
+                for heroe in heroes_mas_bajos:
+                    print(
+                        f'Nombre: {heroe["nombre"]}. Altura: {heroe["altura"]}')
+
+                os.system('pause')
+            else:
+                print(
+                    'Debe calcular primero las alturas maximas y minimas de los heroes (puntos 3 y 4)')
+                os.system('pause')
 
         case '7':
-            pass
+            heroe_mas_pesado = calcular_maximo_en_clave_diccionarios(
+                heroes_normalizados, 'peso')
+            heroe_menos_pesado = calcular_minimo_en_clave_diccionarios(
+                heroes_normalizados, 'peso')
+
+            heroes_mas_pesados = filtrar_lista_diccionarios(
+                heroes_normalizados, 'peso', heroe_mas_pesado)
+            heroes_menos_pesados = filtrar_lista_diccionarios(
+                heroes_normalizados, 'peso', heroe_menos_pesado)
+
+            print('\nHEROE MAS PESADO')
+            mostrar_valores_por_claves(
+                heroes_mas_pesados, ['nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 20)
+            print('\n\n')
+            print('HEROE MENOS PESADO')
+            mostrar_valores_por_claves(
+                heroes_menos_pesados, ['nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 20)
+
+            os.system('pause')
 
         case '8':
-            pass
+            heoroes_genero_m = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'M')
+
+            mostrar_valores_por_claves(heoroes_genero_m, ['nombre'], 20)
+
+            os.system('pause')
 
         case '9':
-            pass
+            heoroes_genero_f = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'F')
+
+            mostrar_valores_por_claves(heoroes_genero_f, ['nombre'], 20)
+
+            os.system('pause')
 
         case '10':
-            pass
+            flag_10 = True
+            heoroes_genero_m = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'M')
+
+            altura_maxima_m = calcular_maximo_en_clave_diccionarios(
+                heoroes_genero_m, 'altura')
+
+            heroes_genero_m_mas_altos = filtrar_lista_diccionarios(
+                heoroes_genero_m, 'altura', altura_maxima_m)
+
+            print('\nHEROE MAS ALTO')
+            mostrar_valores_por_claves(heroes_genero_m_mas_altos, [
+                                       'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 20)
+
+            os.system('pause')
 
         case '11':
-            pass
+            flag_11 = True
+            heoroes_genero_f = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'F')
+
+            altura_maxima_f = calcular_maximo_en_clave_diccionarios(
+                heoroes_genero_f, 'altura')
+
+            heroes_genero_f_mas_altos = filtrar_lista_diccionarios(
+                heoroes_genero_f, 'altura', altura_maxima_f)
+
+            print('\nHEROINA MAS ALTA')
+            mostrar_valores_por_claves(heroes_genero_f_mas_altos, [
+                                       'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 23)
+
+            os.system('pause')
 
         case '12':
-            pass
+            flag_12 = True
+            heoroes_genero_m = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'M')
+
+            altura_minima_m = calcular_minimo_en_clave_diccionarios(
+                heoroes_genero_m, 'altura')
+
+            heroes_genero_m_mas_bajos = filtrar_lista_diccionarios(
+                heoroes_genero_m, 'altura', altura_minima_m)
+
+            print('\nHEROE MAS BAJO')
+            mostrar_valores_por_claves(heroes_genero_m_mas_bajos, [
+                                       'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 20)
+
+            os.system('pause')
 
         case '13':
-            pass
+            flag_13 = True
+            heoroes_genero_f = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'F')
+
+            altura_minima_f = calcular_minimo_en_clave_diccionarios(
+                heoroes_genero_f, 'altura')
+
+            heroes_genero_f_mas_bajos = filtrar_lista_diccionarios(
+                heoroes_genero_f, 'altura', altura_minima_f)
+
+            print('\nHEROINA MAS BAJA')
+            mostrar_valores_por_claves(heroes_genero_f_mas_bajos, [
+                                       'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 20)
+
+            os.system('pause')
 
         case '14':
-            pass
+            heoroes_genero_m = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'M')
+
+            altura_promedio_m = calcular_promedio_en_clave_diccionarios(
+                heoroes_genero_m, 'altura')
+
+            print(
+                f'\nEl promedio de alturas de los superheroes de genero masculino es de: {altura_promedio_m:.2f}\n')
+            os.system('pause')
 
         case '15':
-            pass
+            heoroes_genero_f = filtrar_lista_diccionarios(
+                heroes_normalizados, 'genero', 'F')
+
+            altura_promedio_f = calcular_promedio_en_clave_diccionarios(
+                heoroes_genero_f, 'altura')
+
+            print(
+                f'\nEl promedio de alturas de los superheroes de genero femenino es de: {altura_promedio_f:.2f}\n')
+            os.system('pause')
 
         case '16':
-            pass
+            if flag_10 and flag_11 and flag_12 and flag_13:
+                print('\nHeroe mas alto')
+                mostrar_valores_por_claves(heroes_genero_m_mas_altos, [
+                    'nombre', 'altura'], 15)
+                print('\n------------------------------\n')
+
+                print('Heroina mas alta')
+                mostrar_valores_por_claves(heroes_genero_f_mas_altos, [
+                    'nombre', 'altura'], 15)
+                print('\n------------------------------\n')
+
+                print('Heroe mas bajo')
+                mostrar_valores_por_claves(heroes_genero_m_mas_bajos, [
+                    'nombre', 'altura'], 15)
+                print('\n------------------------------\n')
+
+                print('Heroina mas bajo')
+                mostrar_valores_por_claves(heroes_genero_f_mas_bajos, [
+                    'nombre', 'altura'], 15)
+                print('\n------------------------------\n')
+
+                os.system('pause')
+
+            else:
+                print(
+                    'Debe calcular primero las alturas maximas y minimas de los heroes (puntos 10 al 13)')
+                os.system('pause')
 
         case '17':
-            pass
+            determinar_colores_de_ojo(heroes_normalizados)
+
+            os.system('pause')
 
         case '18':
-            pass
+            determinar_colores_de_pelo(heroes_normalizados)
+
+            os.system('pause')
 
         case '19':
-            pass
+            determinar_tipos_de_inteligencia(heroes_normalizados)
+
+            os.system('pause')
 
         case '20':
-            pass
+            heroes_por_color_de_ojos = duplicar_lista(heroes_normalizados)
+
+            ordenar_diccionarios(
+                lambda heroe1, heroe2: heroe1["color_ojos"] < heroe2["color_ojos"], heroes_por_color_de_ojos)
+
+            mostrar_valores_por_claves(heroes_por_color_de_ojos, [
+                'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 30)
+
+            os.system('pause')
 
         case '21':
-            pass
+            heroes_por_color_de_pelo = duplicar_lista(heroes_normalizados)
+
+            ordenar_diccionarios(
+                lambda heroe1, heroe2: heroe1["color_pelo"] < heroe2["color_pelo"], heroes_por_color_de_pelo)
+
+            mostrar_valores_por_claves(heroes_por_color_de_pelo, [
+                'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 30)
+
+            os.system('pause')
 
         case '22':
-            pass
+            heroes_por_tipo_de_inteligencia = duplicar_lista(
+                heroes_normalizados)
 
-        case '23':
-            pass
+            ordenar_diccionarios(
+                lambda heroe1, heroe2: heroe1["inteligencia"] < heroe2["inteligencia"], heroes_por_tipo_de_inteligencia)
 
-        case '24':
-            pass
+            mostrar_valores_por_claves(heroes_por_tipo_de_inteligencia, [
+                'nombre', 'identidad', 'empresa', 'altura', 'peso', 'genero', 'color_ojos', 'color_pelo', 'fuerza', 'inteligencia'], 30)
+
+            os.system('pause')
 
         case 'x':
             if input('¿CONFIRMAR SALIDA? (X):') == 'si':
